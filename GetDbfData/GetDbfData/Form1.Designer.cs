7e70a71913855c738d0f9cf34e9f46197cbfba8a
tewuapple <576240104@qq.com>
2017-04-28 10:31:53 +0800

    first commit

---
+++ GetDbfData/GetDbfData/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace GetDbfData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 74);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(825, 403);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(200, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 489);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ GetDbfData/GetDbfData/Form1.cs
@@ -0,0 +1,238 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.Odbc;
+using SocialExplorer.IO.FastDBF;
+using System.IO;
+
+namespace GetDbfData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TestReadFile()
+        {
+            string[] args ={ @"C:\Users\ziqiang.sun\Desktop\»ªÁú-0279.dbf", @"C:\Users\ziqiang.sun\Desktop\TestNew2.dbf" };
+            if (args.Length < 2)
+            {
+                //print help
+                Console.WriteLine("\n\n");
+                Console.WriteLine("Welcome to Social Explorer DBF 2 CSV Utility");
+                Console.WriteLine("-------------------------------------------------");
+                Console.WriteLine("\nParameters:");
+                Console.WriteLine("1. input DBF file");
+                Console.WriteLine("2. output CSV file");
+
+                Console.WriteLine("\nOptional switches:");
+                Console.WriteLine("/F  - format numbers so 5.5000 comes out as 5.5");
+                Console.WriteLine("/P  - padded output, fixed width (/P trumps /F)");
+                Console.WriteLine("/Q  - only output quotes when comma appears in data");
+
+                Console.WriteLine("\n\nExample: dbf2csv \"in.dbf\" \"out.csv\" /P /Q");
+
+            }
+            else
+            {
+
+                //check if input DBF file exists...
+                if (!File.Exists(args[0]))
+                {
+                    Console.WriteLine("Input file '" + args[0] + "' does not exist!");
+                    return;
+                }
+
+
+                ////create output csv file overwrite if already exists.
+                //if (File.Exists(args[1]))
+                //{
+                //    //ask to overwrite:
+                //    Console.WriteLine("Output CSV file '" + args[1] + "' already exists.");
+                //    Console.WriteLine("Would you like to overwrite it? Press 'Y' for yes: ");
+                //    if (Console.ReadKey().KeyChar.ToString().ToUpper() != "Y")
+                //        return;
+                //}
+
+                bool bSwitchF = false;
+                bool bSwitchP = false;
+                bool bSwitchQ = false;
+
+                for (int i = 0; i < args.Length; i++)
+                    if (args[i] == "/F")
+                        bSwitchF = true;
+
+                for (int i = 0; i < args.Length; i++)
+                    if (args[i] == "/P")
+                        bSwitchP = true;
+
+                for (int i = 0; i < args.Length; i++)
+                    if (args[i] == "/Q")
+                        bSwitchQ = true;
+
+
+                ////open DBF file and create CSV output file...
+                //StreamWriter swcsv = new StreamWriter(args[1], false, Encoding.Default);
+                //create a simple DBF file and output to args[0]
+                DbfFile odbf = new DbfFile(Encoding.GetEncoding(936));
+                odbf.Open(args[1], FileMode.Create);
+
+                DbfFile dbf = new DbfFile(Encoding.GetEncoding(936));
+                dbf.Open(args[0], FileMode.Open);
+
+
+                ////output column names
+                //for (int i = 0; i < dbf.Header.ColumnCount; i++)
+                //{
+                //    if (dbf.Header[i].ColumnType != DbfColumn.DbfColumnType.Binary &&
+                //       dbf.Header[i].ColumnType != DbfColumn.DbfColumnType.Memo)
+                //        swcsv.Write((i == 0 ? "" : ",") + dbf.Header[i].Name);
+                //    else
+                //        Console.WriteLine("WARNING: Excluding Binary/Memo field '" + dbf.Header[i].Name + "'");
+
+                //}
+
+                //output column names
+                for (int i = 0; i < dbf.Header.ColumnCount; i++)
+                {
+                    odbf.Header.AddColumn(dbf.Header[i]);
+                }
+
+                //swcsv.WriteLine();
+
+                //output values for all but binary and memo...
+                DbfRecord orec = new DbfRecord(dbf.Header);
+                while (dbf.ReadNext(orec))
+                {
+                    //output column values...
+                    if (!orec.IsDeleted)
+                    {
+                        //if (orec["ZQZH"] != this.textBox1.Text.Trim())
+                        //    break;
+                        DbfRecord orec_out = new DbfRecord(odbf.Header);
+                        orec_out.AllowDecimalTruncate = true;
+                        for (int i = 0; i < orec.ColumnCount; i++)
+                        {
+                            orec_out[i] = orec[i].Replace("\0", "");
+                        }
+                        odbf.Write(orec_out);
+                        //for (int i = 0; i < orec.ColumnCount; i++)
+                        //{
+                        //    if (orec.Column(i).ColumnType == DbfColumn.DbfColumnType.Character)
+                        //    {
+                        //        //string values: trim, enclose in quotes and escape quotes with double quotes
+                        //        string sval = orec[i];
+
+                        //        if (!bSwitchP)
+                        //            sval = orec[i].Trim();
+
+                        //        if (!bSwitchQ || sval.IndexOf('"') > -1)
+                        //            sval = ("\"" + sval.Replace("\"", "\"\"") + "\"");
+
+                        //        swcsv.Write(sval);
+
+                        //    }
+                        //    else if (orec.Column(i).ColumnType == DbfColumn.DbfColumnType.Date)
+                        //    {
+                        //        swcsv.Write(orec.GetDateValue(i).ToString("MM-dd-yyyy"));
+                        //    }
+                        //    else
+                        //    {
+                        //        if (bSwitchP)
+                        //            swcsv.Write(orec[i]);
+                        //        else if (bSwitchF)
+                        //            swcsv.Write(FormatNumber(orec[i].Trim()));
+                        //        else
+                        //            swcsv.Write(orec[i].Trim());
+                        //    }
+
+                        //    end record with a linefeed or end column with a comma.
+                        //    if (i < orec.ColumnCount - 1)
+                        //    swcsv.Write(",");
+
+                        //}
+
+                        //write line...
+                        //swcsv.WriteLine();
+
+                    }
+
+                }
+
+
+                //close files...
+                //swcsv.Flush();
+                //swcsv.Close();
+
+                odbf.WriteHeader();
+
+                odbf.Close();
+                dbf.Close();
+
+
+
+            }
+        }
+        /// <summary>
+        /// Removes leading and trailing zeros from a number (double or int) value represented as a string.
+        /// So for example if a '5.00' is passed this function would return '5'. If '00035.3420' is passed, '35.342' is returned.
+        /// etc.
+        /// </summary>
+        /// <param name="sDouble"></param>
+        /// <returns></returns>
+        public static string FormatNumber(string sNumber)
+        {
+            //an empty string is effectively a NULL number, not a zero or anything so just return it as empty.
+            if (sNumber == null || sNumber == "")
+                return "";
+
+            //if this is not a decimal number, remove leading zeros...
+            if (sNumber.IndexOf('.') == -1)
+            {
+                //this is a bit tricky here. we could get a number "00000", which is really "0",
+                sNumber = sNumber.TrimStart("0".ToCharArray());
+
+                //if nothing is left, that means we had a zero to start with! since TrimStart("000") will return "".
+                if (sNumber == "")
+                    sNumber = "0";
+
+                return sNumber;
+
+            }
+            else
+            {
+                string svalFormatted = sNumber;
+                svalFormatted = svalFormatted.Trim().Trim("0".ToCharArray());
+
+                //if only a period is left behind remove it
+                if (svalFormatted != "" && svalFormatted[svalFormatted.Length - 1] == '.')
+                    svalFormatted = svalFormatted.Substring(0, svalFormatted.Length - 1);
+
+                //if formatted number starts with a '.' then add a 0 in front!
+                if (svalFormatted.Length > 0 && svalFormatted[0] == '.')
+                    svalFormatted = "0" + svalFormatted;
+
+                //if nothing is left, that means we had a zero to start with! Trim("0") removed all 
+                //trailing and leading zeros and a period was left over which was then removed as well, 
+                //so the string is empty!
+                if (svalFormatted == "")
+                    svalFormatted = "0";
+
+                return svalFormatted;
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TestReadFile();
+        }
+    }
+}
